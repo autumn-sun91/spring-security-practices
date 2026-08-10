a01d705afa63933e291fad17042c5d0ebfc1406a
autumn-sun91 <dpwe231@gmail.com>
2026-08-10 16:07:17 +0900

    inmemory 유저 추가

--- src/main/java/com/example/securitypractices/config/SecurityConfig.java
+++ src/main/java/com/example/securitypractices/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,5 +29,17 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults()); // 기본적인 인증 시스템을 사용
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // authenticationManager 직접 쓰는거 대신
+        // spring security 6 부터는 AuthenticationManager -> UserDetailsService 순으로 내부적으로 인증 흐름을 갖음.
+        // authenticationManager 경우 custom 으로 사용 e.g.) JwtAuthenticationProvider
+        return new InMemoryUserDetailsManager(
+                // {noop} > spring security 에서 지정해놓은 패스워드 인코더를 사용하는 문법, noop 은 인코더 사용X
+                // role 경우 내부적으로 ROLE_USER, ROLE_ADMIN 으로 지정됨.
+                User.withUsername("thk").password("{noop}123").roles("USER").build(),
+                User.withUsername("admin").password("{noop}!@#").roles("ADMIN").build());
     }
 }
