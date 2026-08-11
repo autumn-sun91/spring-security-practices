9fe14453ce454e6d6fd5d9efed729c49e5538785
autumn-sun91 <dpwe231@gmail.com>
2026-08-11 18:02:11 +0900

    roleHierarchy 추가

--- src/main/java/com/example/securitypractices/config/SecurityConfig.java
+++ src/main/java/com/example/securitypractices/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.password4j.Argon2Function;
 import com.password4j.types.Argon2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +17,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // 5버전에는 AccessDecisionManager 로 관리
+    // 6버전 이후에는 내부적으로 AuthorizationManager 가 관리
+    // beam 으로 등록 시 내부적으로 권한 체크 시 roleHierarchy 도 같이 검사
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_USER");
+    }
+
     // SecurityFilterChain 은 spring security 내부 동작에서 FilterChainProxy 객체에서 filter 를 순회하면서 아래 설정한 값을 적용
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -25,6 +35,8 @@ public class SecurityConfig {
                         .permitAll()
                         .requestMatchers("/admin")
                         .hasRole("ADMIN")
+                        .requestMatchers("/user")
+                        .hasRole("USER")
                         .anyRequest()
                         .authenticated())
                 .formLogin(Customizer.withDefaults()) // security formLogin 을 사용할것인지?
@@ -41,6 +53,6 @@ public class SecurityConfig {
                 4, // Parallelism (threads)
                 32, // Hash length
                 Argon2.ID // Argon2 type (Argon2id is best for general password hashing)
-        ));
+                ));
     }
 }
