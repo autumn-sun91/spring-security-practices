f2540be4521ea0f0ff4a37184f86b5e7d90248c3
autumn-sun91 <dpwe231@gmail.com>
2026-08-12 13:30:17 +0900

    Java Callable 에서 Principal 정보 공유 확인용

--- src/main/java/com/example/securitypractices/form/SampleController.java
+++ src/main/java/com/example/securitypractices/form/SampleController.java
@@ -1,9 +1,12 @@
 package com.example.securitypractices.form;
 
+import com.example.securitypractices.common.SecurityLogger;
 import java.security.Principal;
+import java.util.concurrent.Callable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
@@ -42,5 +45,18 @@ public class SampleController {
     public String user(Model model, Principal principal) {
         model.addAttribute("message", "Hello User, " + principal.getName());
         return "user";
+    }
+
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler() {
+        // async 로 별도 쓰레드로 로직 처리를 한다고 하더라도
+        // WebAsyncManagerIntegrationFilter 를 통해서 같은 Principal 정보를 참조 가능
+        SecurityLogger.log("MVC");
+
+        return () -> {
+            SecurityLogger.log("Callable");
+            return "Async Handler";
+        };
     }
 }
